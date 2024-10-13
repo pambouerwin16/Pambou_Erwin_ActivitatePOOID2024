ab4f0762f40b5e30eca0e1228692d9606768eb07
ErwinPambou <erwinp@superplay.co>
2024-10-13 22:42:12 +0300

    transfer parametrii wip

---
+++ ExempleRecapitulareC02/TransferParametrii.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void interschimabre1(int a, int b)
+{
+    int aux = a;
+    a = b;
+    b = aux;
+}
+
+void interschimbare2(int *a, int *b)
+{
+    
+}
+
+int main(int argc, char* argv[])
+{
+
+}
