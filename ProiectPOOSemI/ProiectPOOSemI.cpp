366d53967243d7215d175c049922fd999157eec7
ErwinPambou <erwinp@superplay.co>
2024-10-30 22:55:31 +0200

    Proiect Faza 2 Complet - Accesati ProiectPOOSemI.cpp
    Deoarece unele cerinte din fazele urmatoare cer modificarea codului scris deja am decis sa urc separat pentru fiecare faza in parte pentru a se putea observa stadiile prin care a trecut acesta la fiecare faza.

--- ProiectPOOSemI/ProiectPOOSemI.cpp
+++ ProiectPOOSemI/ProiectPOOSemI.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <pplwin.h>
 #include <string>
 
+class Proprietate;
+class Parcare;
+class Apartament;
+void AfisareComplex(const Proprietate& p, const Apartament& a, const Parcare& parc);
 using namespace std;
 
 // Cele 3 clase initiale for fi Proprietate, Apartament, Parcare deoarece contin initialele mele P (Pambou) si respectiv E (Erwin-Emmanuel)
@@ -15,9 +20,10 @@ private:
     string locatie;
     int nrFacilitati;
     string* facilitati; //atribut pointer cu memorie pe HEAP
-    //Declaram atributele clasei in zona publica conform cerintei
 
 public:
+    friend void AfisareProprietate(const Proprietate& p);
+    friend void AfisareComplex(const Proprietate& p, const Apartament& a, const Parcare& parc);
     #pragma region Constructor/Destructori
     //Constructor1 - Default - fara parametrii
     Proprietate(): ID(++nrProprietati)
@@ -50,37 +56,30 @@ public:
         }
         delete[] facilitatiprop; //Eliberam memoria alocata pentru vectorul de facilitati primit ca parametru;
     }
+
+    //Copy Constructor
+    Proprietate(const Proprietate& prop): ID(++nrProprietati), pret(prop.pret), locatie(prop.locatie),
+                                          nrFacilitati(prop.nrFacilitati)
+    {
+        facilitati = new string[nrFacilitati];
+        for (int i = 0; i < nrFacilitati; i++)
+        {
+            facilitati[i] = prop.facilitati[i];
+        }
+    }
+
     //Destructor
     ~Proprietate()
     {
-        if(facilitati != nullptr)
+        if (facilitati != nullptr)
         {
             delete[] facilitati;
         }
+        nrProprietati--;
     }
     #pragma endregion
     #pragma region Metode/Functii
     //Metoda pentru afisare a obiectului optionala - pentru a ajuta demonstrarea functionarii constructorilor in main
-    void Afisare()
-    {
-        cout << "Proprietate nr " << ID << ":" << endl;
-        cout << "Pret: " << pret << endl;
-        cout << "Locatie: " << locatie << endl;
-        if (nrFacilitati <= 0 || facilitati == nullptr) //Nu incercam sa afisam facilitatile daca proprietatea nu detine
-        {
-            cout << "Nicio facilitate disponibila" << endl;
-        }
-        else
-        {
-            cout << "Facilitati:" << endl << "{" << endl;
-            for (int i = 0; i < nrFacilitati; i++)
-            {
-                cout << facilitati[i] << endl;
-            }
-            cout << "}" << endl;
-        }
-        cout << endl;
-    }
 
     //Functie statica pentru procesare -> Aplicare discount pe pretul proprietatii
     static void AplicareDiscount(float disc, Proprietate& p)
@@ -97,52 +96,77 @@ public:
     {
         pret = p;
     }
+
     //Setter pentru locatie
     void SetLocatie(string loc)
     {
         locatie = loc;
     }
+
     //Setter pentru numarul facilitatilor
     void SetNrFacilitati(int nrfac)
     {
         nrFacilitati = nrfac;
     }
+
     //Setter pentru facilitati (modifica si numarul facilitatilor)
     void SetFacilitati(string* fc, int nrfc)
     {
-        if(facilitati!= nullptr)
+        if (facilitati != nullptr)
         {
             delete[] facilitati;
         }
-        
+
         facilitati = new string[nrfc];
 
-        for(int i = 0; i < nrfc; i++)
+        for (int i = 0; i < nrfc; i++)
         {
-            facilitati[i]=fc[i];
+            facilitati[i] = fc[i];
         }
         nrFacilitati = nrfc;
         delete[] fc;
     }
+
+    int GetID() const
+    {
+        return ID;
+    }
+
     //Getter pentru pret
     float GetPret()
     {
         return pret;
     }
+
     //Getter pentru locatie
     string GetLocatie()
     {
         return locatie;
     }
+
     //Getter pentru numarul facilitatilor
     int GetNrFacilitati()
     {
         return nrFacilitati;
     }
+
     //Getter pentru facilitati
     string* GetFacilitati() const
     {
-        return facilitati;//Am ales sa returnez un pointer constant la array-ul facilitatilor pentru cateva motive: 1. sa nu fie nevoie de procesare in plus pentru a crea o copie pe care sa o returnez, 2. pentru ca dezalocarea sa revina destructorului clasei si nu la alegerea apelatorului
+        return facilitati;
+        //Am ales sa returnez un pointer constant la array-ul facilitatilor pentru cateva motive: 1. sa nu fie nevoie de procesare in plus pentru a crea o copie pe care sa o returnez, 2. pentru ca dezalocarea sa revina destructorului clasei si nu la alegerea apelatorului
+    }
+
+    //Setter static pentru numarul proprietatilor(atentie cum e folosit, este public deoarece cerinta vrea sa dea acces la acel atribut privat static)
+    static void SetNrProp(const int nrp)
+    {
+        nrProprietati = nrp;
+    }
+
+    //Getter static pentru numarul proprietatilor
+    static int GetNrProp()
+    {
+        return nrProprietati;
     }
     #pragma endregion
 };
@@ -153,8 +177,7 @@ int Proprietate::nrProprietati = 0;
 //Clasa a doua, contine P si E (aPartamEnt)
 class Apartament
 {
-    //Decalram atributele in zona publica
-public:
+private:
     const int ID;
     static int nrApartamente;
     int nrCamere;
@@ -162,6 +185,8 @@ public:
     int nrLocuitori;
     string* locuitori;
 
+public:
+    friend void AfisareComplex(const Proprietate& p, const Apartament& a, const Parcare& parc);
     #pragma region Constructori/Destructori
     //Constructor1 - Default - fara parametrii
     Apartament(): ID(++nrApartamente)
@@ -195,6 +220,28 @@ public:
         }
         delete[] loc; //Eliberm memoria vectorului primit ca parametru alocat in main
     }
+
+    //Copy Constructor
+    Apartament(const Apartament& ap): ID(++nrApartamente), nrCamere(ap.nrCamere), intretinere(ap.intretinere),
+                                      nrLocuitori(ap.nrLocuitori)
+    {
+        locuitori = new string[nrLocuitori];
+
+        for (int i = 0; i < nrLocuitori; i++)
+        {
+            locuitori[i] = ap.locuitori[i];
+        }
+    }
+
+    //Destructor
+    ~Apartament()
+    {
+        if (locuitori != nullptr)
+        {
+            delete[] locuitori;
+        }
+        nrApartamente--;
+    }
     #pragma endregion
     #pragma region Metode/Functii
     //Functie statica pentru afisare, primeste ca parametru obiectul care vrem sa fie afisat
@@ -226,27 +273,30 @@ public:
     {
         nrCamere = nr;
     }
+
     //Setter pentru intretinere
     void SetIntretinere(float suma)
     {
         intretinere = suma;
     }
+
     //Setter pentru numarul locuitorilor
     void SetNrLocuitori(int nr)
     {
         nrLocuitori = nr;
     }
+
     //Setter pentru locuitori (modifica si numarul locuitorilor)
     void SetLocuitori(string* loc, int nrlc)
     {
-        if(locuitori != nullptr)
+        if (locuitori != nullptr)
         {
             delete[] locuitori;
         }
 
         locuitori = new string[nrlc];
 
-        for(int i = 0; i < nrlc; i++)
+        for (int i = 0; i < nrlc; i++)
         {
             locuitori[i] = loc[i];
         }
@@ -254,25 +304,46 @@ public:
 
         delete[] loc;
     }
+
+    int GetID() const
+    {
+        return ID;
+    }
+
     //Getter pentru numarul camerelor
     int GetNrCamere()
     {
         return nrCamere;
     }
+
     //Getter pentru suma intretinere
     float GetIntretinere()
     {
         return intretinere;
     }
+
     //Getter pentru numarul locuitorilor
     int NrLocuitori()
     {
         return nrLocuitori;
     }
+
     //Getter pentru locuitori
     string* GetLocuitori() const
     {
         return locuitori;
+    }
+
+    //Setter static pentru numarul apartamentelor
+    static void SetNrApartamente(int nr)
+    {
+        nrApartamente = nr;
+    }
+
+    //Getter static pentru numarul apartamentelor
+    static int GetNrApartamente()
+    {
+        return nrApartamente;
     }
     #pragma endregion
 };
@@ -283,15 +354,17 @@ int Apartament::nrApartamente = 0;
 //Clasa 3 - contine P si E (ParcarE)
 class Parcare
 {
-    //Declaram atributele in zona publica conform cerintei
-public:
+private:
     const int ID; //atribut constant
     static int nrParcari; //atribut static
     unsigned int capacitateLocuri;
     unsigned int locuriOcupate;
     unsigned int* idMasini;
-    //Atribut pointer ce va aloca spatiu un HEAP - id-ul propriul fiecarei masini, nu id pentru evidenta
 
+public:
+    friend void AfisareComplex(const Proprietate& p, const Apartament& a, const Parcare& parc);
+    //Atribut pointer ce va aloca spatiu un HEAP - id-ul propriul fiecarei masini, nu id pentru evidenta
+    #pragma region Constructori/Destructori
     //Constructor1 - Default - fara parametrii
     Parcare(): ID(++nrParcari)
     {
@@ -321,6 +394,29 @@ public:
         }
     }
 
+    //Copy Constructor
+    Parcare(const Parcare& parc): ID(++nrParcari), capacitateLocuri(parc.capacitateLocuri),
+                                  locuriOcupate(parc.locuriOcupate)
+    {
+        idMasini = new unsigned int[locuriOcupate];
+
+        for (int i = 0; i < locuriOcupate; i++)
+        {
+            idMasini[i] = parc.idMasini[i];
+        }
+    }
+
+    //Destructor
+    ~Parcare()
+    {
+        if (idMasini != nullptr)
+        {
+            delete[] idMasini;
+        }
+        nrParcari--;
+    }
+    #pragma endregion
+    #pragma region Metode/Functii
     //Functie statica pentru procesare - pentru evacuare masina specifica din parcare
     static void EvacuareMasina(unsigned int idMas, Parcare& p)
     {
@@ -393,10 +489,166 @@ public:
         }
         cout << endl;
     }
+    #pragma endregion
+    #pragma region Getteri/Setteri
+    //Setter pentru capacitate locuri
+    void SetCapacitateLocuri(unsigned int caploc)
+    {
+        capacitateLocuri = caploc;
+    }
+
+    //Setter pentru locuri ocupate
+    void SetLocuriOcupate(unsigned int lococup)
+    {
+        locuriOcupate = lococup;
+    }
+
+    //Setter pentru masini
+    void SetIdMasini(unsigned int* listaid, int ocup)
+    {
+        if (idMasini != nullptr)
+        {
+            delete[] idMasini;
+        }
+
+        idMasini = new unsigned int[ocup];
+
+        for (int i = 0; i < ocup; i++)
+        {
+            idMasini[i] = listaid[i];
+        }
+
+        locuriOcupate = ocup;
+    }
+
+    int GetID() const
+    {
+        return ID;
+    }
+
+    //Getter pentru capacitatea locurilor
+    unsigned int GetCapacitateLocuri()
+    {
+        return capacitateLocuri;
+    }
+
+    //Getter pentru numarul locurilor ocupate
+    unsigned int GetLocuriOcupate()
+    {
+        return locuriOcupate;
+    }
+
+    //Getter pentru idurile masinilor din parcare
+    unsigned int* GetIdMasini() const
+    {
+        return idMasini;
+    }
+
+    //Setter static pentru numarul parcarilor
+    static void SetNrParcari(int nr)
+    {
+        nrParcari = nr;
+    }
+
+    //Getter static pentru numarul parcarilor
+    static int GetNrParcari()
+    {
+        return nrParcari;
+    }
+    #pragma endregion
 };
 
 //Initializam atributul static
 int Parcare::nrParcari = 0;
+
+
+void AfisareProprietate(const Proprietate& p)
+{
+    cout << "Proprietatea nr " << p.ID << ":" << endl;
+    cout << "Pret: " << p.pret << endl;
+    cout << "Locatie: " << p.locatie << endl;
+    cout << "Numar Facilitati: " << p.nrFacilitati << endl;
+    cout << "Facilitati:" << endl;
+    if (p.nrFacilitati > 0 || p.facilitati != nullptr)
+    {
+        cout << "{" << endl;
+        for (int i = 0; i < p.nrFacilitati; i++)
+        {
+            cout << p.facilitati[i] << endl;
+        }
+        cout << "}" << endl;
+    }
+    else
+    {
+        cout << "Nicio facilitate disponibila..." << endl;
+    }
+    cout << endl;
+}
+
+void AfisareComplex(const Proprietate& p, const Apartament& a, const Parcare& parc)
+{
+    cout << "Informatii complex:" << endl;
+
+    cout << "Proprietatea nr " << p.ID << ":" << endl;
+    cout << "Pret: " << p.pret << endl;
+    cout << "Locatie: " << p.locatie << endl;
+    cout << "Numar Facilitati: " << p.nrFacilitati << endl;
+    cout << "Facilitati:" << endl;
+    if (p.nrFacilitati > 0 || p.facilitati != nullptr)
+    {
+        cout << "{" << endl;
+        for (int i = 0; i < p.nrFacilitati; i++)
+        {
+            cout << p.facilitati[i] << endl;
+        }
+        cout << "}" << endl;
+    }
+    else
+    {
+        cout << "Nicio facilitate disponibila..." << endl;
+    }
+    cout << endl;
+
+    cout << "Apartamentul nr " << a.ID << ":" << endl;
+    cout << "Numar Camere: " << a.nrCamere << endl;
+    cout << "Intretinere: " << a.intretinere << endl;
+    cout << "Numar Locuitori: " << a.nrLocuitori << endl;
+    cout << "Facilitati:" << endl;
+    if (a.nrLocuitori > 0 || a.locuitori != nullptr)
+    {
+        cout << "{" << endl;
+        for (int i = 0; i < a.nrLocuitori; i++)
+        {
+            cout << a.locuitori[i] << endl;
+        }
+        cout << "}" << endl;
+    }
+    else
+    {
+        cout << "Apartamentul nu este locuit..." << endl;
+    }
+    cout << endl;
+
+    cout << "Parcarea nr " << parc.ID << endl;
+    cout << "Capacitate parcare: " << parc.capacitateLocuri << endl;
+    cout << "Numar locuri de parcare ocupate: " << parc.locuriOcupate << endl;
+    cout << "Masini parcate (ID-uri):" << endl;
+    if (parc.locuriOcupate > 0 || parc.idMasini != nullptr)
+    {
+        cout << "{" << endl;
+        for (int i = 0; i < parc.locuriOcupate; i++)
+        {
+            cout << parc.idMasini[i] << endl;
+        }
+        cout << "}" << endl;
+    }
+    else
+    {
+        cout << "Parcarea este goala..." << endl;
+    }
+    cout << endl;
+}
+
 
 void main()
 {
@@ -405,27 +657,38 @@ void main()
     Proprietate prop2("Str. Panselelor nr. 12, Bl. Bla, Ap. Bla");
     Proprietate prop3;
 
-    Proprietate::AplicareDiscount(20, prop1);
-
-    prop1.Afisare();
-    prop2.Afisare();
-    prop3.Afisare();
-
     Apartament ap1(3, 443.27f, 3, new string[3]{"Popescu Mariana", "Popescu Beniamin", "Popescu Popescu"});
     Apartament ap2(3);
     Apartament ap3;
 
-    Apartament::Afisare(ap1);
-    Apartament::Afisare(ap2);
-    Apartament::Afisare(ap3);
 
-    Parcare parcare1(10, 4, new unsigned int[4]{1234, 435, 562, 435});
+    Parcare parcare1(10, 3, new unsigned int[3]{1234, 435, 562});
     Parcare parcare2(12);
     Parcare parcare3;
 
+    cout << "Initial avem " << Proprietate::GetNrProp() << " proprietati, " << Apartament::GetNrApartamente() <<
+        " apartamente si " << Parcare::GetNrParcari() << " parcari." << endl << endl;
+
+    AfisareProprietate(prop1);
+    AfisareProprietate(prop2);
+    AfisareProprietate(prop3);
+
+    Proprietate::AplicareDiscount(20, prop1);
+
+    cout << "Initial parcarea nr " << parcare1.GetID() << " contine " << parcare1.GetLocuriOcupate() <<
+        " masini parcate. ID-urile masinilor parcate sunt:";
+    const unsigned int* masini = parcare1.GetIdMasini();
+    for (int i = 0; i < parcare1.GetLocuriOcupate(); i++)
+    {
+        cout << " " << masini[i];
+        if (i != parcare1.GetLocuriOcupate() - 1)
+        {
+            cout << ",";
+        }
+    }
+    cout << "." << endl << endl;
+
     Parcare::EvacuareMasina(435, parcare1);
 
-    parcare1.Afisare();
-    parcare2.Afisare();
-    parcare3.Afisare();
+    AfisareComplex(prop1, ap1, parcare1);
 }
